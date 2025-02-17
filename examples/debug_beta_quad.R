12513fa66f3bc5351f2bde69cc481c0d85ff558f
Jeffrey S. Racine <racinej@mcmaster.ca>
2025-02-17 12:16:22 -0500

    All good, pushing prelim to GItHub and emailing Jackson

--- examples/debug_beta_quad.R
+++ examples/debug_beta_quad.R
@@ -1,32 +1,33 @@
 library(bkcde)
 library(progress)
 progress.bar <- TRUE
-set.seed(42)
+## set.seed(42)
 n <- scan("n.dat")
 M <- scan("M.dat")
-theta <- 110
-phi <- 50
+theta <- 160
+phi <- 40
 if(progress.bar) pbb <- progress_bar$new(format = "  Monte Carlo Simulation [:bar] :percent eta: :eta",
                                          clear = TRUE,
                                          force = TRUE,
                                          width = 60,
                                          total = M)
-par(mfrow=c(2,2),cex=.75)
 for(m in 1:M) {
   if(!progress.bar) cat("\r",m," of ",M," replications",sep="")
   x <- sort(runif(n,0,1))
   y <- rbeta(n,1+x^2,2-x^2)
+  ## change dgp in two places, after call to f and here
   dgp.mean <- (1+x^2)/(1+2)
-  f.ml <- bkcde(x=x, y=y,,degree.min=2,degree.max=2,proper=TRUE)
-  plot(unique(f.ml$x.eval),f.ml$g)
-  lines(x,dgp.mean)
-  plot(unique(f.ml$x.eval),f.ml$g,type="l")
-  #f.ml <- bkcde(x=x, y=y,y.ub=Inf,y.lb=-Inf,x.ub=Inf,x.lb=-Inf)
-  dgp <-dbeta(f.ml$y.eval,1+f.ml$x.eval,2-f.ml$x.eval)
-  zlim <- range(f.ml$f,dgp,0,2)
-  plot(f.ml,zlim=zlim,theta=theta,phi=phi,main=paste("CV degree = ", f.ml$degree,", sf.y = ", formatC(f.ml$h.sf[1],format="f",digits=2), ", sf.x = ", formatC(f.ml$h.sf[2],format="f",digits=2)))
-  persp(unique(f.ml$x.eval), theta=theta,phi=phi,unique(f.ml$y.eval), matrix(dgp,f.ml$n.grid,f.ml$n.grid), main="DGP",
+  f <- bkcde(x=x, y=y,degree.min=0,degree.max=3,proper=TRUE,x.lb=0,x.ub=1,y.lb=0,y.ub=1)
+  ylim <- range(f$g,dgp.mean)
+  par(mfrow=c(2,2),cex=.75)
+  dgp <- dbeta(f$y.eval,1+f$x.eval^2,2-f$x.eval^2)
+  zlim <- range(f$f,dgp,0,2)
+  plot(f,zlim=zlim,theta=theta,phi=phi,main=paste("CV degree = ", f$degree,", sf.y = ", formatC(f$h.sf[1],format="f",digits=2), ", sf.x = ", formatC(f$h.sf[2],format="f",digits=2)))
+  persp(unique(f$x.eval), theta=theta,phi=phi,unique(f$y.eval), matrix(dgp,f$n.grid,f$n.grid), main=paste("DGP, n = ",n,sep=""),
         ticktype="detailed", xlab="x", ylab="y", zlab="f(y|x)",zlim=zlim)
+  plot(unique(f$x.eval),f$g,ylim=ylim,xlab="x",ylab="g(x)",main=paste("Conditional mean g(x) vs x, n = ",n,sep=""))
+  lines(x,dgp.mean,col=2)
+  legend("topleft",legend=c("g(x)","DGP"),lty=1,col=c(1,2),bty="n")
   if(progress.bar) pbb$tick()  
 }
 
--- examples/debug_norm_lin.R
+++ examples/debug_norm_lin.R
@@ -1,37 +1,33 @@
-### Well, this looks OK though with large x get a bias in the tails...
-
 library(bkcde)
 library(progress)
 progress.bar <- TRUE
-set.seed(42)
+## set.seed(42)
 n <- scan("n.dat")
 M <- scan("M.dat")
-theta <- 110
-phi <- 50
+theta <- 170
+phi <- 60
 if(progress.bar) pbb <- progress_bar$new(format = "  Monte Carlo Simulation [:bar] :percent eta: :eta",
                                          clear = TRUE,
                                          force = TRUE,
                                          width = 60,
                                          total = M)
-par(mfrow=c(2,2),cex=.75)
 for(m in 1:M) {
   if(!progress.bar) cat("\r",m," of ",M," replications",sep="")
   x <- sort(runif(n,0,1))
   y <- rnorm(n,x,sd=sqrt(1/12))
   dgp.mean <- x
-  #f <- bkcde(h=c(.5*sd(y)*n^{-1/6},10),degree=1,x=x, y=y,degree.min=1,degree.max=1,proper=TRUE)#,y.lb=-Inf,y.ub=Inf,x.lb=-Inf,x.ub=Inf)
-  #f <- bkcde(h=c(.20*sd(y)*n^{-1/6},1*sd(x)*n^{-1/6}),degree=1,x=x, y=y,degree.max=5,degree.min=0,proper=TRUE)#,y.lb=-Inf,y.ub=Inf)#,x.lb=-Inf,x.ub=Inf)
-  f <- bkcde(h=c(.20*sd(y)*n^{-1/6},1000*sd(x)*n^{-1/6}),degree=1,x=x, y=y,degree.max=1,degree.min=1,proper=TRUE,y.trim=-0.25,x.trim=-0.25)
+  #f <- bkcde(h=c(.5*sd(y)*n^{-1/6},10*sd(x)*n^{-1/6}),degree=1,y.erf=.5,integrate.erf=1,n.integrate=1000,x.erf=0,x=x, y=y,degree.min=0,degree.max=3,proper=TRUE,x.lb=-Inf,x.ub=Inf,y.lb=-Inf,y.ub=Inf)
+  f <- bkcde(h=c(.5*sd(y)*n^{-1/6},1000*sd(x)*n^{-1/6}),degree=2,x=x, y=y,proper=TRUE,degree.min=1,degree.max=1,y.lb=-Inf,y.ub=Inf,y.erf=.5)
   ylim <- range(f$g,dgp.mean)
-  plot(unique(f$x.eval),f$g,ylim=ylim)
-  lines(x,dgp.mean)
-  plot(unique(f$x.eval),f$g,type="l")
-  #f <- bkcde(x=x, y=y,y.ub=Inf,y.lb=-Inf,x.ub=Inf,x.lb=-Inf)
+  par(mfrow=c(2,2),cex=.75)
   dgp <-dnorm(f$y.eval,f$x.eval,sd=sqrt(1/12))
   zlim <- range(f$f,dgp,0,2)
   plot(f,zlim=zlim,theta=theta,phi=phi,main=paste("CV degree = ", f$degree,", sf.y = ", formatC(f$h.sf[1],format="f",digits=2), ", sf.x = ", formatC(f$h.sf[2],format="f",digits=2)))
-  persp(unique(f$x.eval), theta=theta,phi=phi,unique(f$y.eval), matrix(dgp,f$n.grid,f$n.grid), main="DGP",
+  persp(unique(f$x.eval), theta=theta,phi=phi,unique(f$y.eval), matrix(dgp,f$n.grid,f$n.grid), main=paste("DGP, n = ",n,sep=""),
         ticktype="detailed", xlab="x", ylab="y", zlab="f(y|x)",zlim=zlim)
+  plot(unique(f$x.eval),unique(f$g),ylim=ylim,xlab="x",ylab="g(x)",main=paste("Conditional mean g(x) vs x, n = ",n,sep=""),type="l")
+  lines(x,dgp.mean,col=2)
+  legend("topleft",legend=c("g(x)","DGP"),lty=1,col=c(1,2),bty="n")
   if(progress.bar) pbb$tick()  
 }
 
